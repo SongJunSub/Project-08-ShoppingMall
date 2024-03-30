8e9b30785c835347e6115660cbb1fd39e3d7bc1a
SongJunSub <s911130@naver.com>
2024-03-31 02:42:56 +0900

    1. Lombok 추가 2. Entity 추가 3. HTML에 서버 데이터 전송

--- src/main/java/com/shoppingmall/controller/ItemController.java
+++ src/main/java/com/shoppingmall/controller/ItemController.java
@@ -1,15 +1,25 @@
 package com.shoppingmall.controller;
 
+import com.shoppingmall.entity.Item;
+import com.shoppingmall.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
+
+    private final ItemRepository itemRepository;
 
     @GetMapping("/list")
     public String list(Model model){
-        model.addAttribute("name", "SongJunSub");
+        List<Item> result = itemRepository.findAll();
+
+        model.addAttribute("items", result);
 
         return "list";
     }
---
+++ src/main/java/com/shoppingmall/repository/ItemRepository.java
@@ -0,0 +1,10 @@
+package com.shoppingmall.repository;
+
+import com.shoppingmall.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+
+
+}
